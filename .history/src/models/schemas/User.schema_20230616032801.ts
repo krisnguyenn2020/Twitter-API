fa8cffe8a339bd8118fb92fbc04bbb69c1b37b3a
Kris Nguyen <krisnguyenn2020@gmail.com>
2023-06-16 03:43:57 -0400

    Hash password

--- .history/src/models/schemas/User.schema_20230616032801.ts
+++ .history/src/models/schemas/User.schema_20230616032801.ts
@@ -1,8 +1,11 @@
 // Use Class over Interface because of recreating an object faster
 import { ObjectId } from 'mongodb'
-import { UserVerifyStatus } from '~/constants/enums'
 
-
+enum UserVerifyStatus {
+  Unverified, // chưa xác thực email, mặc định = 0
+  Verified, // đã xác thực email
+  Banned // bị khóa
+}
 interface UserType {
   _id?: ObjectId
   name: string
@@ -56,7 +59,7 @@ export default class User {
     this.bio = user.bio || ''
     this.location = user.location || ''
     this.website = user.website || ''
-    this.username = user.username || ''
+    this.username = user.username
     this.avatar = user.avatar || ''
     this.cover_photo = user.cover_photo || ''
   }
---
+++ src/models/requests/User.requests.ts
@@ -0,0 +1,9 @@
+// declare interface
+
+export interface RegisterReqBody {
+  name: string
+  email: string
+  password: string
+  confirm_password: string
+  date_of_birth: string
+}
